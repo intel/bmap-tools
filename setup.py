67984660f19154eb16612495fcd1fab5097074cc
Artem Bityutskiy <artem.bityutskiy@intel.com>
2013-01-07 10:21:54 +0200

    Pre-release 2.0-rc4
    Change-Id: I0f848639ca9b12cd2ca14bfa0523df729b4b5358
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@intel.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description = "Bmap tools",
     author = "Artem Bityutskiy",
     author_email = "artem.bityutskiy@linux.intel.com",
-    version = "2.0-rc3",
+    version = "2.0-rc4",
     scripts = ['bmaptool'],
     packages = find_packages(exclude = [ "test*" ]),
     license='GPLv2',
