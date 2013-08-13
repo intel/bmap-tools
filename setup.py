622b286a032ec7a0e53bfdaff080f777f6fac820
Artem Bityutskiy <artem.bityutskiy@intel.com>
2013-08-13 14:56:24 +0300

    Release version 2.6
    Change-Id: Ic0aba0b20847454b35c7dee9d50dccdbd2cf11fa
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@intel.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="Bmap tools",
     author="Artem Bityutskiy",
     author_email="artem.bityutskiy@linux.intel.com",
-    version="2.5",
+    version="2.6",
     scripts=['bmaptool'],
     packages=find_packages(exclude=["test*"]),
     license='GPLv2',
