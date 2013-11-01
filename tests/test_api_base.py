3cc38eaab141dab343e312ccc16288b6193b1be8
Artem Bityutskiy <artem.bityutskiy@intel.com>
2013-11-01 14:14:40 +0200

    tests: add proper license header to all tests
    Change-Id: I95195b227301119486c967cea2a338391ff26123
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@intel.com>

--- tests/test_api_base.py
+++ tests/test_api_base.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2012-2013 Intel, Inc.
+# License: GPLv2
+# Author: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
 """
 This test verifies the base bmap creation and copying API functionality. It
 generates a random sparse file, then creates a bmap fir this file and copies it
