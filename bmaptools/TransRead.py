intel/bmap-tools

commit 6343239fb2487c309c9573ee35e7832b08753180

TransRead: try to have the child process read the compressed file directly
I've had problems with the read thread not terminating when expected
and the main thread blocking in join() as a result. This simplifies
the situation significantly: the read thread is only used when we're
using urllib to download a file.

Signed-off-by: Simon McVittie <smcv@debian.org>
Reviewed-by: Alexander D. Kanevskiy <kad@kad.name>

bmaptools/TransRead.py
@@ -342,16 +342,23 @@ class TransRead(object):
             args = archiver + " " + args
         else:
             args = decompressor + " " + args
+
+        if hasattr(self._f_objs[-1], 'fileno'):
+            child_stdin = self._f_objs[-1].fileno()
+        else:
+            child_stdin = subprocess.PIPE
+
         child_process = subprocess.Popen(args, shell=True,
                                          bufsize=1024*1024,
-                                         stdin=subprocess.PIPE,
+                                         stdin=child_stdin,
                                          stdout=subprocess.PIPE,
                                          stderr=subprocess.PIPE)
 
-        args = (self._f_objs[-1], child_process.stdin, )
-        self._rthread = threading.Thread(target=self._read_thread, args=args)
-        self._rthread.daemon = True
-        self._rthread.start()
+        if child_stdin == subprocess.PIPE:
+            args = (self._f_objs[-1], child_process.stdin, )
+            self._rthread = threading.Thread(target=self._read_thread, args=args)
+            self._rthread.daemon = True
+            self._rthread.start()
 
         self._force_fake_seek = True
         self._f_objs.append(child_process.stdout)

commit 2e51863cf86b631327535cf1619f42f71f002189

TransRead: unbreak reading compressed file from an URL
This patch fixes a regression introduced by commit 6343239fb2487c309c9573ee35e7832b08753180 -
reading a compressed file from an URL stopped working. Apparently urllib's file-like objects do have
a 'fileno', but reading from it gives EOF immediately. So lets use 'is_url' to make a decision about
whether we create a reader thread or not.

Change-Id: I6ba9570f823f3943f202d4b0170782a74aa0333e
Signed-off-by: Artem Bityutskiy <artem.bityutskiy@intel.com>

bmaptools/TransRead.py
@@ -380,10 +380,10 @@ class TransRead(object):
         else:
             args = decompressor + " " + args
 
-        if hasattr(self._f_objs[-1], 'fileno'):
-            child_stdin = self._f_objs[-1].fileno()
-        else:
+        if self.is_url:
             child_stdin = subprocess.PIPE
+        else:
+            child_stdin = self._f_objs[-1].fileno()
 
         child_process = subprocess.Popen(args, shell=True,
                                          bufsize=1024 * 1024,
@@ -392,6 +392,8 @@ class TransRead(object):
                                          stderr=subprocess.PIPE)
 
         if child_stdin == subprocess.PIPE:
+            # A separate reader thread is created only when we are reading via
+            # liburl.
             args = (self._f_objs[-1], child_process.stdin, )
             self._rthread = threading.Thread(target=self._read_thread, args=args)
             self._rthread.daemon = True